2de1b9f53fb982e240fd1eaab58eafa25f93e203
SarimDx <82496250+SarimDx@users.noreply.github.com>
2021-08-18 09:34:40 +0200

    buliding a scroling x in progress

--- Main.js
+++ Main.js
@@ -41,28 +41,27 @@ window.addEventListener("scroll", () => {
   if (window.scrollY <= 100) {
     Button.classList.add("Button");
     Button.classList.remove("button_apear");
-  }
-  else  {
+  } else {
     Button.classList.remove("Button");
   }
 
-// ? Click Button Action
-let lastBcscroll = window.scrollY;
-Button.addEventListener("click", () => {
-  window.scrollBy(0, -lastBcscroll);
-  lastBcscroll = window.scrollY;
-});
+  // ? Click Button Action
+  let lastBcscroll = window.scrollY;
+  Button.addEventListener("click", () => {
+    window.scrollBy(0, -lastBcscroll);
+    lastBcscroll = window.scrollY;
+  });
 });
 
 // ? Dark/light Mode
 let DButton = document.querySelector(".DarkMode");
 let LButton = document.querySelector(".lightMode");
 DButton.addEventListener("click", () => {
-    document.documentElement.style.setProperty("--main-color", "#552208");
-    document.documentElement.style.setProperty("--second-color", "#083B55");
-    document.documentElement.style.setProperty("--writing-color", "#ffffff");
-    LButton.style.display = "block";
-    DButton.style.display = "none";
+  document.documentElement.style.setProperty("--main-color", "#552208");
+  document.documentElement.style.setProperty("--second-color", "#083B55");
+  document.documentElement.style.setProperty("--writing-color", "#ffffff");
+  LButton.style.display = "block";
+  DButton.style.display = "none";
 });
 LButton.addEventListener("click", () => {
   document.documentElement.style.setProperty("--main-color", "#5d4954");
@@ -72,26 +71,24 @@ LButton.addEventListener("click", () => {
   DButton.style.display = "block";
 });
 
-
-
-// ? Sort Button 
-let AllButton = document.querySelector(".AllButton")
-let All = document.querySelectorAll(".box")
-let AnimeButton = document.querySelector(".AnimeButton")
+// ? Sort Button
+let AllButton = document.querySelector(".AllButton");
+let All = document.querySelectorAll(".box");
+let AnimeButton = document.querySelector(".AnimeButton");
 let Anime = document.querySelectorAll(".Anime");
-let PhotoButton = document.querySelector(".PhotoButton")
-let Photo = document.querySelectorAll(".Photo")
-let NatureButton = document.querySelector(".NatureButton")
-let Nature = document.querySelectorAll(".Nature")
-let ArtButton = document.querySelector(".ArtButton")
-let Art = document.querySelectorAll(".Art")
+let PhotoButton = document.querySelector(".PhotoButton");
+let Photo = document.querySelectorAll(".Photo");
+let NatureButton = document.querySelector(".NatureButton");
+let Nature = document.querySelectorAll(".Nature");
+let ArtButton = document.querySelector(".ArtButton");
+let Art = document.querySelectorAll(".Art");
 let moreButton = document.querySelector(".more");
 let lessButton = document.querySelectorAll(".More");
 let lessButton2 = document.querySelector(".less");
 AllButton.addEventListener("click", () => {
-  for(let i = 0 ; i< All.length ; i++){
+  for (let i = 0; i < All.length; i++) {
     All[i].style.display = "block";
-   }
+  }
   moreButton.style.display = "none";
   lessButton2.style.display = "block";
   AllButton.classList.add("active");
@@ -99,17 +96,16 @@ AllButton.addEventListener("click", () => {
   PhotoButton.classList.remove("active");
   NatureButton.classList.remove("active");
   ArtButton.classList.remove("active");
-})
+});
 
-  AnimeButton.addEventListener("click", () => {
-    console.log("Anime")
-    for(let i = 0 ; i< All.length ; i++){
-      if(All[i].classList.contains('Anime') == true) {
-        All[i].style.display = "block";
-      }
-      else {
-        All[i].style.display = "none";
-      }
+AnimeButton.addEventListener("click", () => {
+  console.log("Anime");
+  for (let i = 0; i < All.length; i++) {
+    if (All[i].classList.contains("Anime") == true) {
+      All[i].style.display = "block";
+    } else {
+      All[i].style.display = "none";
+    }
   }
   moreButton.style.display = "none";
   lessButton2.style.display = "none";
@@ -118,14 +114,13 @@ AllButton.addEventListener("click", () => {
   PhotoButton.classList.remove("active");
   NatureButton.classList.remove("active");
   ArtButton.classList.remove("active");
-})
+});
 PhotoButton.addEventListener("click", () => {
-  console.log("Photo")
-  for(let i = 0 ; i< All.length ; i++){
-    if(All[i].classList.contains('Photo') == true) {
+  console.log("Photo");
+  for (let i = 0; i < All.length; i++) {
+    if (All[i].classList.contains("Photo") == true) {
       All[i].style.display = "block";
-    }
-    else {
+    } else {
       All[i].style.display = "none";
     }
   }
@@ -136,15 +131,14 @@ PhotoButton.addEventListener("click", () => {
   PhotoButton.classList.add("active");
   NatureButton.classList.remove("active");
   ArtButton.classList.remove("active");
-})
+});
 NatureButton.addEventListener("click", () => {
-  console.log("Nature")
-  for(let i = 0 ; i< All.length ; i++){
-    if(All[i].classList.contains('Nature') == true) {
-     All[i].style.display = "block";
-    }
-    else {
-     All[i].style.display = "none";
+  console.log("Nature");
+  for (let i = 0; i < All.length; i++) {
+    if (All[i].classList.contains("Nature") == true) {
+      All[i].style.display = "block";
+    } else {
+      All[i].style.display = "none";
     }
   }
   moreButton.style.display = "none";
@@ -154,14 +148,13 @@ NatureButton.addEventListener("click", () => {
   PhotoButton.classList.remove("active");
   NatureButton.classList.add("active");
   ArtButton.classList.remove("active");
-})
+});
 ArtButton.addEventListener("click", () => {
-  console.log("Art")
-  for(let i = 0 ; i< All.length ; i++){
-    if(All[i].classList.contains('Art') == true) {
+  console.log("Art");
+  for (let i = 0; i < All.length; i++) {
+    if (All[i].classList.contains("Art") == true) {
       All[i].style.display = "block";
-    }
-    else {
+    } else {
       All[i].style.display = "none";
     }
   }
@@ -172,22 +165,22 @@ ArtButton.addEventListener("click", () => {
   PhotoButton.classList.remove("active");
   NatureButton.classList.remove("active");
   ArtButton.classList.add("active");
-})
+});
 
-// ? More/less Button 
+// ? More/less Button
 moreButton.addEventListener("click", () => {
-  for(let i = 0 ; i< All.length ; i++){
+  for (let i = 0; i < All.length; i++) {
     All[i].style.display = "block";
   }
   moreButton.style.display = "none";
   lessButton2.style.display = "block";
-})
+});
 lessButton2.addEventListener("click", () => {
-  for(let i = 0 ; i< All.length ; i++){
-    if(All[i].classList.contains('More') == true) {
-     All[i].style.display = "none";
+  for (let i = 0; i < All.length; i++) {
+    if (All[i].classList.contains("More") == true) {
+      All[i].style.display = "none";
     }
   }
   moreButton.style.display = "block";
   lessButton2.style.display = "none";
-})
+});
